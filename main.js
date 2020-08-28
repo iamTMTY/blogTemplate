24c556d7b1eb3a4dee7c5e9fd8d1f1bd68146cdc
TMTY <salaudeen.t@gmail.com>
2020-08-28 19:11:16 +0100

    added functionality

---
+++ main.js
@@ -0,0 +1,46 @@
+const menu = document.querySelector('.ham-menu'); 
+const nav = document.querySelector('.mobile-nav');
+const dashes = document.querySelectorAll('.dash');
+
+document.querySelectorAll('.post').forEach( a => a.addEventListener('click', () => document.location.href = 'post.html'))
+
+menu.addEventListener('click', () => {
+
+    let navOpen = menu.classList.contains('nav-open')
+
+    if (!navOpen) {
+        nav.style.top = '0'
+        nav.style.marginBottom = '0'
+        menu.classList.remove('nav-close')
+        menu.classList.add('nav-open')
+        dashes[0].style.transform = 'rotate(34deg)'
+        dashes[1].style.left = '-30px'
+        dashes[2].style.transform = 'rotate(-34deg)'
+    } 
+            
+    if (navOpen) {
+        nav.style.top = '-100px'
+        nav.style.marginBottom = '-100px'
+        menu.classList.add('nav-close')
+        menu.classList.remove('nav-open')
+        dashes[0].style.transform = 'rotate(0deg)'
+        dashes[1].style.left = '0'
+        dashes[2].style.transform = 'rotate(-0deg)'
+
+    }
+})
+
+document.querySelectorAll('.pop_up').forEach(el => {
+    el.addEventListener('click', () => {
+        let form = document.createElement('form')
+        // console.log(window.scrollY);
+        document.querySelector('.contact').style.top = `${window.scrollY}px`
+        document.querySelector('.contact').style.height = '100vh'
+        document.querySelector('body').style.overflowY = 'hidden'
+    })
+})
+
+document.querySelector('.close').addEventListener('click', () => {
+    document.querySelector('.contact').style.height = '0'
+    document.querySelector('body').style.overflowY = 'visible'
+})
